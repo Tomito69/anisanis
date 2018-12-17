5f88c8170e5c7bcaa9d87ec4631facaa2dac7095
pkpkp <45944561+pkpkp@users.noreply.github.com>
2018-12-17 16:38:46 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("523808032921092116")
+setInterval(function() {
+channel.send(`-_-_-_-_-_-__-_-_-_-__-_-_-_-__-_-_-_ggajgjahrjè_zpdz_çtjpf)e=igàagkaglagagagla)gkiaga`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
